5c77645823bd987a263fba661c34c1ac702f9d93
Aaron Rodriquez <azr.rodriquez@gmail.com>
2019-09-23 16:41:57 -0700

    changed welcome message

--- CH03/basic.py
+++ CH03/basic.py
@@ -1,3 +1,5 @@
-print("Hello and Welcome to a basic python program!")
+# print("Hello and Welcome to a basic python program!")
+import sys
+print ("Hello, " + sys.argv[1] + "and Welcome to a basic python program")
 food = input("What is your favorite food?")
 print("Your favorite food is "+ food + "? I might like it too")
