4f78d624f89f69ab342fa62569adb3388e53b63c
Aaron Rodriquez <azr.rodriquez@gmail.com>
2019-09-24 16:15:15 -0700

    added code

--- CH03/which_seasons.py
+++ CH03/which_seasons.py
@@ -1,7 +1,7 @@
 print('Welcome to the which season program')
 
 month_string = input('Input the month number (e.g. January==1): ')
-day_string = input('Input the day of the month (e.g. 19)')
+day_string = input('Input the day of the month (e.g. 19): ')
 
 month = int(month_string)
 day = int(day_string)
@@ -15,4 +15,27 @@ elif month == 7 or month == 8 or month == 9:
 else:
     season = 'autumn'
 
+
+if (month == 3) and (day > 19):
+    season = 'spring'
+elif (month == 6) and (day < 20):
+    season = 'summer'
+elif (month == 9) and (day > 21):
+    season = 'autumn'
+elif (month == 12) and (day > 20):
+    season = 'winter'
+
 print("Season is ", season)
+
+#BONUS MATERIAL
+
+print(input("What is your favorite month? (e.g. May == 5)"))
+
+if (month == 9):
+    print("September is my favorite too")
+elif (month > 9):
+    print("Those are nice too, but cold")
+elif (month < 9):
+    print("Winter, Spring and Summer are great")
+else:
+    print("That's not a month i know of...")
